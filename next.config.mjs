defc8c567e14200ecad8b4b40bf86680022ecc4a
Ronit Goel <110287876+ronitgoel@users.noreply.github.com>
2024-08-28 12:53:31 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,15 @@ const nextConfig = {
         ]
     },
     typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
         ignoreBuildErrors: true,
       },
+      eslint: {
+        ignoreDuringBuilds: true
+      }
 };
 
 export default nextConfig;
