11040f245fa9ebddfb31780b50958be0acd9ab1e
Ronit Goel <110287876+ronitgoel@users.noreply.github.com>
2024-08-28 16:42:43 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -14,40 +14,35 @@ interface HomeProps {
 const Home = async ({searchParams}: HomeProps) => {
   const isEmpty = true;
 
-  try {
-    const listings = await getListings(searchParams);
-    const currentUser = await getCurrentUser();
-  
-    if (listings.length === 0) {
-      return (
-        <ClientOnly>
-          <EmptyState
-            showReset
-          />
-        </ClientOnly>
-      )
-    }
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
     return (
       <ClientOnly>
-        <Container>
-          <div className="pt-24 grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            {listings.map((listing: any) => {
-              return (
-                <ListingCard
-                  currentUser={currentUser}
-                  key={listing.id}
-                  data={listing}
-                />
-              )
-            })}
-          </div>
-        </Container>
+        <EmptyState
+          showReset
+        />
       </ClientOnly>
-    );
-  } catch(error: any)
-  {
-    throw new Error(error);
+    )
   }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            )
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
 
 export default Home;
